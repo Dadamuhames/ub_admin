56f78d94d411e46f453b6c1d3ae8aefb2d1696c7
MSD <dadamuhames@gmail.com>
2023-02-28 22:33:31 +0500

    first

--- admins/views.py
+++ admins/views.py
@@ -17,8 +17,7 @@ class BasedListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(BasedListView, self).get_context_data(**kwargs)
 
-        context['objects'] = get_lst_data(
-            self.get_queryset(), self.request, 20)
+        context['objects'] = get_lst_data(self.get_queryset(), self.request, 20)
         context['page_obj'] = paginate(self.get_queryset(), self.request, 20)
         context['url'] = search_pagination(self.request)
 
@@ -122,6 +121,25 @@ class QuoteGroupCreate(CreateView):
     success_url = '/admins/quotes_group'
 
 
+    def form_valid(self, form):
+        return None
+
+
+    def post(self, request, *args, **kwargs):
+        context = super().post(request, *args, **kwargs)
+        name = request.POST.get("name")
+        quotes = request.POST.get("quotes")
+
+
+
+
+
+        return 
+
+
+
+
+
 # quote group edit
 class QuoteGroupEdit(UpdateView):
     model = SearchQuoteGroup
@@ -150,3 +168,224 @@ def delete_item(request):
         pass
 
     return redirect(url)
+
+
+# translations list
+class TranslationList(ListView):
+    model = Translations
+    template_name = 'admin/translation_list.html'
+
+    def get_queryset(self):
+        queryset = Translations.objects.order_by("-id")
+        query = self.request.GET.get("q")
+        queryset = search_translation(query, queryset)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(TranslationList, self).get_context_data(**kwargs)
+        context['groups'] = TranlsationGroups.objects.all()
+        context['langs'] = Languages.objects.filter(
+            active=True).order_by('-default')
+        context['url'] = search_pagination(self.request)
+
+        # pagination
+        context['translations'] = get_lst_data(
+            self.get_queryset(), self.request, 20)
+        context['page_obj'] = paginate(self.get_queryset(), self.request, 20)
+
+        return context
+
+
+# translation group
+class TranslationGroupDetail(DetailView):
+    model = TranlsationGroups
+    template_name = 'admin/translation_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TranslationGroupDetail,
+                        self).get_context_data(**kwargs)
+        context['groups'] = TranlsationGroups.objects.all()
+        context['langs'] = Languages.objects.filter(
+            active=True).order_by('-default')
+        lst_one = self.get_object().translations.order_by('-id')
+
+        # search
+        query = self.request.GET.get("q")
+        lst_one = search_translation(query, lst_one)
+
+        # range
+        lst_two = range(1, len(lst_one) + 1)
+
+        # zip 2 lst
+        context['translations'] = dict(pairs=zip(lst_one, lst_two))
+
+        return context
+
+
+# transtion update
+def translation_update(request):
+    if request.method == 'GET':
+        id = request.GET.get('id')
+
+        try:
+            translation = Translations.objects.get(id=int(id))
+            serializer = TranslationSerializer(translation)
+
+            return JsonResponse(serializer.data)
+        except:
+            return JsonResponse({'error': 'error'}, safe=False)
+
+    elif request.method == 'POST':
+        data = serialize_request(Translations, request)
+        id = request.POST.get("id")
+        lang = Languages.objects.filter(
+            active=True).filter(default=True).first()
+
+        if data.get('value').get(lang.code, '') == '':
+            return JsonResponse({'lng_error': 'This language is required'})
+
+        try:
+            translation = Translations.objects.get(id=int(id))
+            key = data.get('key', '')
+
+            if key == '':
+                return JsonResponse({'key_error': 'Key is required'})
+
+            if str(key) in [str(it.key) for it in Translations.objects.filter(group=translation.group).exclude(id=translation.pk)]:
+                return JsonResponse({'key_error': 'Key is already in use'})
+
+            translation.key = key
+            translation.value = data['value']
+            translation.full_clean()
+            translation.save()
+        except:
+            return JsonResponse('some error', safe=False)
+
+        serializer = TranslationSerializer(translation)
+
+        return JsonResponse(serializer.data)
+
+
+# add translation group
+def add_trans_group(request):
+    if request.method == 'POST':
+        data_dict = serialize_request(TranlsationGroups, request)
+
+        if data_dict.get('sub_text', '') == '':
+            return JsonResponse({'key_error': 'Sub text is required'})
+        elif (data_dict.get('sub_text'), ) in TranlsationGroups.objects.values_list('sub_text'):
+            return JsonResponse({'key_error': 'This key is already in use'})
+
+        try:
+            transl_group = TranlsationGroups(**data_dict)
+            transl_group.full_clean()
+            transl_group.save()
+        except ValidationError:
+            return JsonResponse({'title_error': 'This title is empty or already in use'})
+
+        data = {
+            'id': transl_group.id,
+            'name': transl_group.title,
+            'key': transl_group.sub_text
+        }
+        return JsonResponse(data)
+
+
+# translation group udate
+class TranslationGroupUdpate(UpdateView):
+    model = TranlsationGroups
+    template_name = 'admin/translation_edit.html'
+    fields = '__all__'
+    success_url = '/admin/translations'
+
+    def get_context_data(self, **kwargs):
+        context = super(TranslationGroupUdpate,
+                        self).get_context_data(**kwargs)
+        context['groups'] = TranlsationGroups.objects.all()
+        context['langs'] = Languages.objects.filter(
+            active=True).order_by('-default')
+        context['lng'] = Languages.objects.filter(
+            active=True).filter(default=True).first()
+        lst_one = self.get_object().translations.all()
+
+        # range
+        lst_two = range(1, len(lst_one) + 1)
+
+        # zip 2 lst
+        context['translations'] = dict(pairs=zip(lst_one, lst_two))
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        transls = list(self.get_object().translations.all())
+        langs = Languages.objects.filter(active=True).order_by('-default')
+        lang = Languages.objects.filter(
+            active=True).filter(default=True).first()
+        items_count = request.POST.get("item_count")
+
+        data = []
+        for l in range(1, int(items_count) + 1):
+            new_data = {}
+            new_data['id'] = l
+            new_data['key'] = request.POST[f'key[{l}]']
+            new_data['values'] = []
+            for lng in langs:
+                new_data['values'].append(
+                    {'key': f'value[{l}][{lng.code}]', 'value': request.POST[f'value[{l}][{lng.code}]'], 'def_lang': lang.code, 'lng': lng.code})
+
+            data.append(new_data)
+
+        objects = dict(pairs=zip(data, list(range(1, int(items_count) + 1))))
+
+        for i in range(len(transls)):
+            transls[i].key = request.POST.get(f'key[{i + 1}]', '')
+
+            if transls[i].key == '':
+                return render(request, template_name=self.template_name, context={'key_errors': {str(i+1): 'Key is required'},  'new_objects': objects, 'langs': langs, 'len': int(items_count) + 1})
+
+            in_default_lng = request.POST.get(f'value[{i+1}][{lang.code}]', '')
+
+            if in_default_lng == '':
+                return render(request, template_name=self.template_name, context={'lng_errors': {str(i+1): 'This language is required'}, 'new_objects': objects, 'langs': langs, 'len': int(items_count) + 1})
+
+            value_dict = {}
+            for lang in langs:
+                value_dict[str(lang.code)
+                           ] = request.POST[f'value[{i + 1}][{lang.code}]']
+
+            transls[i].value = value_dict
+            try:
+                transls[i].full_clean()
+                transls[i].save()
+            except:
+                return render(request, template_name=self.template_name, context={'key_errors': {str(i): 'Key is alredy in use'},  'new_objects': objects, 'langs': langs, 'len': items_count})
+
+        for i in range(len(transls) + 1, int(items_count) + 1):
+            new_trans = Translations()
+            data = {}
+            new_trans.key = request.POST.get(f'key[{i}]', '')
+
+            if new_trans.key == '':
+                return render(request, template_name=self.template_name, context={'key_errors': {str(i): 'Key is required'},  'new_objects': objects, 'langs': langs, 'len': items_count})
+
+            value_dict = {}
+            in_default_lng = request.POST.get(f'value[{i}][{lang.code}]', '')
+
+            if in_default_lng == '':
+                return render(request, template_name=self.template_name, context={'lng_errors': {str(i): 'This language is required'}, 'new_objects': objects, 'langs': langs, 'len': items_count})
+
+            for lang in langs:
+                value_dict[str(lang.code)
+                           ] = request.POST[f'value[{i}][{lang.code}]']
+
+            new_trans.value = value_dict
+            new_trans.group = self.get_object()
+
+            try:
+                new_trans.full_clean()
+                new_trans.save()
+            except:
+                return render(request, template_name=self.template_name, context={'key_errors': {str(i): 'Key is alredy in use'}, 'new_objects': objects, 'langs': langs, 'len': items_count})
+
+        return redirect('transl_group_detail', pk=self.get_object().id)
